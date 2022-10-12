1403b523c26839ba2855a1a80621c6e298d7214c
mcoswel <mcosweljs@gmail.com>
2022-10-12 17:01:57 +0800

    first

--- src/main/java/NetWork.java
+++ src/main/java/NetWork.java
@@ -1,9 +1,10 @@
+import classes.WheelParam;
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryonet.EndPoint;
 
 public class NetWork {
     public static void register(EndPoint endPoint) {
         Kryo kryo = endPoint.getKryo();
-
+        kryo.register(WheelParam.class);
     }
 }
---
+++ src/main/java/classes/WheelParam.java
@@ -0,0 +1,18 @@
+package classes;
+
+public class WheelParam {
+    private int value;
+    private String name;
+    public int getValue() {
+        return value;
+    }
+    public void setValue(int value) {
+        this.value = value;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
